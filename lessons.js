a1f2dec3d21847c7d4a3e54faca8c5661c02050c
Chris-edappady <chris.edappady123@gmail.com>
2025-10-28 19:47:58 +0400

    Update 5: Adding multiple lessons with v-for loop

--- lessons.js
+++ lessons.js
@@ -3,5 +3,15 @@ let lessons = [
   "subject":"Math", 
   "location":"California", 
   "price":100, 
-  "spaces":5 }
+  "spaces":5 },
+{ "id": 2, 
+  "subject":"Physics", 
+  "location":"Arizona", 
+  "price":120, 
+  "spaces":5},
+{ "id": 3, 
+  "subject":"Chemistry", 
+  "location":"New York", 
+  "price":95, 
+  "spaces":5}
 ];
